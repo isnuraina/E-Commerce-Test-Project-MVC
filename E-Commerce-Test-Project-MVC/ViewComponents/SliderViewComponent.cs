995d86cc709c8309d811851f0b7ef3dc851445b2
isnuraina <nurane.ismayilzade19@gmail.com>
2025-07-23 15:02:14 +0400

    ViewComponents is okey

---
+++ E-Commerce-Test-Project-MVC/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce_Test_Project_MVC.Data;
+using E_Commerce_Test_Project_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_Test_Project_MVC.ViewComponents
+{
+    public class SliderViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public SliderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Slider> sliders = await _context.Sliders.AsNoTracking().ToListAsync();
+            return await Task.FromResult(View(sliders));
+        }
+    }
+}
