382dd004c2f3df338050c53e5dc98e0d9dd7005b
isnuraina <nurane.ismayilzade19@gmail.com>
2025-07-24 11:35:47 +0400

    Category and Products CRUD is READY !!!

--- E-Commerce-Test-Project-MVC/Areas/Admin/Controllers/ProductController.cs
+++ E-Commerce-Test-Project-MVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
-﻿using E_Commerce_Test_Project_MVC.Data;
+﻿using E_Commerce_Test_Project_MVC.Areas.Admin.ViewModels.Product;
+using E_Commerce_Test_Project_MVC.Data;
+using E_Commerce_Test_Project_MVC.Helpers.Extensions;
+using E_Commerce_Test_Project_MVC.Models;
 using E_Commerce_Test_Project_MVC.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
@@ -11,11 +15,13 @@ namespace E_Commerce_Test_Project_MVC.Areas.Admin.Controllers
     {
         private readonly IProductService _productService;
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public ProductController(IProductService productService, AppDbContext context)
+        public ProductController(IProductService productService, AppDbContext context, IWebHostEnvironment env)
         {
             _productService = productService;
             _context = context;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -39,6 +45,63 @@ namespace E_Commerce_Test_Project_MVC.Areas.Admin.Controllers
                 return NotFound();
             }
             return View(product);
+        }
+        [HttpGet]
+        public async Task< IActionResult> Create()
+        {
+            ViewBag.categories = await GetCategoriesAsync();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>Create(ProductCreateVM request)
+        {
+           ViewBag.categories = await GetCategoriesAsync();
+            if (!ModelState.IsValid)
+            {
+                return View(request);
+            }
+
+            ICollection<ProductImage> images = new List<ProductImage>();
+
+
+            foreach (var item in request.Images)
+            {
+                if (item.CheckFileSize(400))
+                {
+                    ModelState.AddModelError("Image", "Image size must be max 400KB");
+                    return View(request);
+                }
+
+                if (!item.CheckFileType("image/"))
+                {
+                    ModelState.AddModelError("Image", "Image type must be image format");
+                    return View(request);
+                }
+                string fileName = Guid.NewGuid().ToString() + "-" + item.FileName;
+                string path = Path.Combine(_env.WebRootPath, "assets", "img", fileName);
+                using (FileStream stream = new FileStream(path, FileMode.Create))
+                {
+                    await item.CopyToAsync(stream);
+                }
+                images.Add(new ProductImage { Img = fileName });
+            }
+            images.FirstOrDefault().IsMain = true;
+            Product product = new()
+            {
+                Name = request.Name,
+                Description = request.Desc,
+                Price = request.Price,
+                CategoryId = request.CategoryId,
+                ProductImages = images
+            };
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private async Task<IEnumerable<SelectListItem>> GetCategoriesAsync()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return categories.Select(a => new SelectListItem() { Value = a.Id.ToString(), Text = a.Name });
 
         }
     }
---
+++ E-Commerce-Test-Project-MVC/Areas/Admin/ViewModels/Product/ProductCreateVM.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_Test_Project_MVC.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_Test_Project_MVC.Areas.Admin.ViewModels.Product
+{
+    public class ProductCreateVM
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+
+        public string Desc { get; set; }
+        [Required]
+
+        public int Price { get; set; }
+        [Required]
+
+        public List<IFormFile> Images { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
